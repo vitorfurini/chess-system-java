9e22889259f512706fc68be956f5c76c8b975932
Vitor Furini <vitor.furini@ntconsult.com.br>
2020-02-17 15:30:39 -0300

    first commit with class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-
-	}
+    public static void main(String[] args) {
+		Position pos = new Position(3,5);
+		System.out.println(pos);
+    }
 
 }
